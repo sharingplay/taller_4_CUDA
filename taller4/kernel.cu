cb566b20c6a65292a8810b55abce42915e170bca
Mario Araya <marioaxtrem@gmail.com>
2023-05-05 11:05:11 -0600

    move project to git

---
+++ taller4/kernel.cu
@@ -0,0 +1,99 @@
+﻿/*
+#include <stdio.h>
+#include <cuda.h>
+
+int* a, * b;  // host data
+int* c, * c2;  // results
+
+//Cuda error checking - non mandatory
+void cudaCheckError() {
+    cudaError_t e = cudaGetLastError();
+    if (e != cudaSuccess) {
+        printf("Cuda failure %s:%d: '%s'\n", __FILE__, __LINE__, cudaGetErrorString(e));
+        exit(0);
+    }
+}
+
+//GPU kernel 
+__global__
+void vecAdd(int* A, int* B, int* C, int N) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    C[i] = A[i] + B[i];
+}
+
+//CPU function
+void vecAdd_h(int* A1, int* B1, int* C1, int N) {
+    for (int i = 0; i < N; i++)
+        C1[i] = A1[i] + B1[i];
+}
+
+int main(int argc, char** argv)
+{
+    printf("Begin \n");
+    //Iterations
+    int n = 50000000;
+    //Number of blocks
+    int nBytes = n * sizeof(int);
+    //Block size and number
+    int block_size, block_no;
+
+    //memory allocation	
+    a = (int*)malloc(nBytes);
+    b = (int*)malloc(nBytes);
+    c = (int*)malloc(nBytes);
+    c2 = (int*)malloc(nBytes);
+
+    int* a_d, * b_d, * c_d;
+    block_size = 500; //threads per block
+    block_no = n / block_size;
+
+    //Work definition
+    dim3 dimBlock(block_size, 1, 1);
+    dim3 dimGrid(block_no, 1, 1);
+
+    // Data filling
+    for (int i = 0; i < n; i++)
+        a[i] = i, b[i] = i;
+
+
+    printf("Allocating device memory on host..\n");
+    //GPU memory allocation
+    cudaMalloc((void**)&a_d, n * sizeof(int));
+    cudaMalloc((void**)&b_d, n * sizeof(int));
+    cudaMalloc((void**)&c_d, n * sizeof(int));
+
+    printf("Copying to device..\n");
+    cudaMemcpy(a_d, a, n * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(b_d, b, n * sizeof(int), cudaMemcpyHostToDevice);
+
+    clock_t start_d = clock();
+    printf("Doing GPU Vector add\n");
+    vecAdd << <block_no, block_size >> > (a_d, b_d, c_d, n);
+    cudaCheckError();
+
+    //Wait for kernel call to finish
+    cudaThreadSynchronize();
+
+    clock_t end_d = clock();
+
+
+    printf("Doing CPU Vector add\n");
+    clock_t start_h = clock();
+    vecAdd_h(a, b, c2, n);
+    clock_t end_h = clock();
+
+    //Time computing
+    double time_d = (double)(end_d - start_d) / CLOCKS_PER_SEC;
+    double time_h = (double)(end_h - start_h) / CLOCKS_PER_SEC;
+
+    //Copying data back to host, this is a blocking call and will not start until all kernels are finished
+    cudaMemcpy(c, c_d, n * sizeof(int), cudaMemcpyDeviceToHost);
+    printf("n = %d \t GPU time = %f \t CPU time = %f\n", n, time_d, time_h);
+
+    //Free GPU memory
+    cudaFree(a_d);
+    cudaFree(b_d);
+    cudaFree(c_d);
+    return 0;
+}
+*/
---
+++ taller4/multMat.cu
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <cuda.h>
+
+#define N 4
+
+__global__ 
+void matrix_mult(int* a, int* b, int* c) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (i < N && j < N) {
+        int sum = 0;
+        for (int k = 0; k < N; k++) {
+            sum += a[i * N + k] * b[k * N + j];
+        }
+        c[i * N + j] = sum;
+    }
+}
+
+int main(int argc, char** argv) {
+    int* a, * b, * c;         // host data
+    int* d_a, * d_b, * d_c;   // device data
+
+    int size = N * N * sizeof(int);
+
+    // Allocate memory on host
+    a = (int*)malloc(size);
+    b = (int*)malloc(size);
+    c = (int*)malloc(size);
+
+    // Matrix values
+    int a_values[N][N] = {{10, 20, 30, 40},
+                          {55, 65, 75, 85},
+                          {96, 106, 116, 126},
+                          {137, 147, 158, 168}};
+
+    int b_values[N][N] = {{12, 23, 32, 43},
+                          {52, 62, 72, 82},
+                          {94, 104, 115, 125},
+                          {139, 149, 150, 160}};
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            a[i * N + j] = a_values[i][j];
+            b[i * N + j] = b_values[i][j];
+        }
+    }
+
+    // Allocate memory on device
+    cudaMalloc(&d_a, size);
+    cudaMalloc(&d_b, size);
+    cudaMalloc(&d_c, size);
+
+    // Copy data from host to device
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+
+    // Set grid and block sizes
+    dim3 dimBlock(2, 2);
+    dim3 dimGrid((N + dimBlock.x - 1) / dimBlock.x, (N + dimBlock.y - 1) / dimBlock.y);
+
+    // Launch kernel
+    matrix_mult << <dimGrid, dimBlock >> > (d_a, d_b, d_c);
+
+    // Wait for kernel to finish
+    cudaDeviceSynchronize();
+
+    // Copy data from device to host
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+
+    // Print result
+    printf("Result:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", c[i * N + j]);
+        }
+        printf("\n");
+    }
+
+    // Free memory
+    free(a);
+    free(b);
+    free(c);
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    return 0;
+}
